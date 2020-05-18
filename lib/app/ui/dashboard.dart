d7eec2ade25f5159e2a4d91314db678e6aea8c9d
Garrett <gsharp.impact@gmail.com>
2020-05-18 11:48:36 -0500

    implement error handling (#1)
    add error handling when making api calls
      - show specific error message when network connection is offline
      - show generic message when error is indeterminate
      - support platform-specific alert dialog UI

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repository.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/ui/endpoint_card.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/ui/last_updated_status_text.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/ui/show_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,9 +18,26 @@ class _DashboardState extends State<Dashboard> {
   EndpointsData _endpointsData;
 
   Future<void> _updateData() async {
-    final dataRepository = Provider.of<DataRepository>(context, listen: false);
-    final endpointsData = await dataRepository.getAllEndpointData();
-    setState(() => _endpointsData = endpointsData);
+    try {
+      final dataRepository =
+          Provider.of<DataRepository>(context, listen: false);
+      final endpointsData = await dataRepository.getAllEndpointData();
+      setState(() => _endpointsData = endpointsData);
+    } on SocketException catch (_) {
+      showAlertDialog(
+        context: context,
+        title: 'Connection Error',
+        content: 'Could not retrieve data.  Please try again later.',
+        defaultActionText: 'Okay',
+      );
+    } catch (_) {
+      showAlertDialog(
+        context: context,
+        title: 'Unknown Error',
+        content: 'Please contact support or try again later.',
+        defaultActionText: 'Okay',
+      );
+    }
   }
 
   @override
